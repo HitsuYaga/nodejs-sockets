7fa95718e6cc22c941604835177a692d659f48c0
dq.tho89 <dq.tho89@gmail.com>
2017-10-16 15:03:54 +0700

    Completely styling project

--- public/js/app.js
+++ public/js/app.js
@@ -1,39 +1,45 @@
-var name = getQueryVariable('name') || 'Anynomous';
-var room = getQueryVariable('room')
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
 var socket = io();
 
 console.log(name + ' wants to join ' + room);
+
+// Update h1 tag
 jQuery('.room-title').text(room);
 
-socket.on('connect', () => {
-  console.log("Connected to the socket io server!")
-  socket.emit('joinRoom', {
-    name: name,
-    room: room
-  })
-})
+socket.on('connect', function () {
+	console.log('Conncted to socket.io server!');
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
+});
 
-socket.on('message', (message) => {
-  var momentTimestamp = moment.utc(message.timestamp)
-  console.log("New message: ");
-  console.log(message.text);
-  $message = jQuery('.message');
-  $message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>')
-  $message.append('<p>' + message.text + '</p>')
-})
+socket.on('message', function (message) {
+	var momentTimestamp = moment.utc(message.timestamp);
+	var $messages = jQuery('.messages');
+	var $message = jQuery('<li class="list-group-item"></li>');
 
-// Hdndle submit message
+	console.log('New message:');
+	console.log(message.text);
+
+	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
+	$message.append('<p>' + message.text + '</p>');
+	$messages.append($message);
+});
+
+// Handles submitting of new message
 var $form = jQuery('#message-form');
 
-$form.on('submit', (event) => {
-  event.preventDefault();
+$form.on('submit', function (event) {
+	event.preventDefault();
 
-  var $message = $form.find('input[name=message]');
+	var $message = $form.find('input[name=message]');
 
-  socket.emit('message', {
-    name: name,
-    text: $message.val()
-  });
+	socket.emit('message', {
+		name: name,
+		text: $message.val()
+	});
 
-  $message.val('');
+	$message.val('');
 });
